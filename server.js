2e4bcd8085fbc0453923b0a833064cb2f765f5f2
India <dabon.india@gmail.com>
2019-10-09 15:31:41 +0200

    new branch created

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs');
 const app = express();
 
-//app.use(express.urlencoded());
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
@@ -10,14 +10,25 @@ app.use(express.static('public'));
 
 
 app.post('/login', function (request, response) {
-    // response.render('/login',{ name: request.body.name });
+
+    const login = request.body.login;
+    const password = request.body.password;
+    let responseData = {
+        status: 0,
+        message: null
+    }
 
 
-    console.log(request.body);
-    const login = request.body.login
-    const password = request.body.password
-    
-    response.send(login + ' ' + password)
+    if (login === 'tata' && password === 'candy') {
+        responseData.status = 200;
+        responseData.message = login;
+
+    } else {
+        responseData.status = 401;
+        responseData.message = 'not authorized';
+
+    }
+    response.status(responseData.status).send(responseData.message);
 
 })
 
@@ -32,7 +43,7 @@ app.get('/titi/', function (request, response) {
     response.send('Hello World titi!!!')
 })
 
-app.get('/users/:id' , function (request, response) {
+app.get('/users/:id', function (request, response) {
     const firstname = request.params.firstname;
     const result = 'Hello ' + firstname;
     response.send(result);
@@ -55,7 +66,7 @@ app.get('/search/q', function (request, response) {
 
 })
 
-app.get('/hello/names' , function (request, response) {
+app.get('/hello/names', function (request, response) {
     console.log(request.query)
     const firstname = request.query.firstname;
     const lastname = request.query.lastname;
@@ -75,5 +86,5 @@ console.dir(req.query.shoe.type)
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
